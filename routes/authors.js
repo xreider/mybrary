4351515f75ab892c41116f48ee49509fc2a5ea83
unknown <xreider@yandex.ru>
2020-04-10 16:33:37 +1000

    good

--- routes/authors.js
+++ routes/authors.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
     try {
         const authors = await Author.find(searchOptions)
         res.render('authors/index', {authors,
-            searchOptions: req.query});
+                                    searchOptions: req.query});
     } catch (error) {
         res.redirect('/')
     }
